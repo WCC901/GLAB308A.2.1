500ea36a00eb624d31f72c5b05ca0c6e880d36c7
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2024-01-05 01:35:51 -0500

    Completed part 2 and gave the two companions variable names so they can be called easier

--- adventure.js
+++ adventure.js
@@ -1,30 +1,57 @@
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            inventory: ["small hat", "sunglasses"]
-        }
-    },
+// Part 1
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//         name: "Leo",
+//         type: "Cat",
+//         companion: {
+//             name: "Frank",
+//             type: "Flea",
+//             inventory: ["small hat", "sunglasses"]
+//         }
+//     },
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//     }
+// }
+
+// Console logs all the items in the adventurer's inventory
+// function logInventory(...inventoryList) {
+//     let itemString = "";
+//     for (const item of inventoryList) {
+//         itemString += item
+//     }
+//     console.log(itemString.replaceAll(","," "));
+// }
+
+// logInventory(adventurer.inventory);
+// adventurer.roll();
+
+// Part 2
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
     }
 }
 
-// Console logs all the items in the adventurer's inventory
-function logInventory(...inventoryList) {
-    let itemString = "";
-    for (const item of inventoryList) {
-        itemString += item
-    }
-    console.log(itemString.replaceAll(","," "));
-}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+const leo = robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+const frank = robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-logInventory(adventurer.inventory);
-adventurer.roll();
+robin.roll();
+leo.roll();
+frank.roll();
